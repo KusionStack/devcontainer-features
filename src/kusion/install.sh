f23053d96bc82c4ecbd30e33cdde15d98a43ea5d
xiarui.xr <xiarui.xr@antgroup.com>
2023-06-03 22:17:50 +0800

    install kcl-language-server to /home/vscode/.kusion/

--- src/kusion/install.sh
+++ src/kusion/install.sh
@@ -6,6 +6,8 @@ set -eux
 VERSION=${VERSION:-"0.8.1"}
 KUSION_URL="https://github.com/KusionStack/kusion/releases/download/v${VERSION}/kusion_${VERSION}_linux_amd64.tar.gz"
 KUSION_LOCATION="/home/vscode/.kusion"
+# todo: this should be installed directly from the kcl language server release
+KCLVM_URL="https://github.com/KusionStack/kcl/releases/download/v0.5.0-alpha.1/kclvm-v0.5.0-alpha.1-linux-amd64.tar.gz"
 
 USERNAME="${USERNAME:-"${_REMOTE_USER:-"automatic"}"}"
 
@@ -30,6 +32,13 @@ mkdir -p ${KUSION_LOCATION}
 # install Kusion pack
 wget -c  ${KUSION_URL} -qO - | tar xz -C ${KUSION_LOCATION}
 # the kusion bin location is added to path by devcontainer-feature.json file
+
+# install kcl language server
+# todo: this should be installed directly from the kcl language server release
+mkdir ${KUSION_LOCATION}/kcl
+wget -c  ${KCLVM_URL} -qO - | tar xz -C ${KUSION_LOCATION}/kcl
+cp ${KUSION_LOCATION}/kcl/kclvm/bin/kcl-language-server ${KUSION_LOCATION}
+rm -rf ${KUSION_LOCATION}/kcl
 
 # ensure that the kusion install path has correct privs
 chown -R $USERNAME ${KUSION_LOCATION}
